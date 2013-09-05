9ecb376bb97f1d70afd98f1fa920c43fe93d64df
cassiesancartier <cassie.sancartier@gmail.com>
2013-09-05 19:37:24 -0400

    submission 9/5/13

--- chap10.py
+++ chap10.py
@@ -1,21 +1,36 @@
 # Name: Cassie Sancartier
 
 
-# 10.7
+# 10.7 - Anagrams
 
 def is_anagram(word1, word2):
-    for letter in word1:
-        if letter not in word2:
-            return False
-    return True
-    
+    word_one_list = list(word1)
+    word_two_list = list(word2)
 
-result:
+    if sorted(word_one_list) == sorted(word_two_list):
+        return True
 
->>> is_anagram('baseball', 'bat')
-False
->>> is_anagram('take', 'teak')
-True
+    else:
+        return False
 
 
-#
+# 10.13 - Interlocking words
+
+
+def does_interlock(word_list):
+    words = set(line.strip() for line in word_list)
+    for w in words:
+        word1, word2 = w[::2], w[1::2]
+    if word1 in words and word2 in words:
+        print word1, word2
+
+
+def does_interlock(word_list):
+    words = set(line.strip() for line in word_list)
+    for w in words:
+        word1, word2, word3 = w[::3], w[1::3], w[2::3]
+    if word1 in words and word2 in words and word3 in words:
+        print word1, word2, word3        
+
+
+
